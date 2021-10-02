afe349650691ee45b455ea697a57ab17d7b8307c
solomon-data-ml <solomon.mailbx@gmail.com>
2021-10-03 00:38:21 +0530

    Cleanups
    Cleanups

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,13 @@ $(document).ready(function () {
     $('.loader').hide();
     $('#result').hide();
 
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#imageUpload").change(function () {
+     
+    $("#csvUpload").change(function () {
         $('.image-section').show();
         $('#btn-predict').show();
         $('#result').text('');
         $('#result').hide();
-        readURL(this);
+        //readURL(this);
     });
 
     // Predict
